345c30bd9187e85010cea0c8ab6204e45f6e3854
ipconfiger <superpowerlee@gmail.com>
2013-03-14 23:19:29 +0800

    asdada

--- app.py
+++ app.py
@@ -1,2 +1,35 @@
 #coding=utf8
 __author__ = 'alex'
+import sys
+import os
+import logging
+import redis
+from flask import Flask, g, session, request, render_template
+import settings
+from flask.ext.script import Manager
+from utils import *
+
+app = Flask("yun")
+app.config.from_object(settings)
+manager = Manager(app)
+
+cache = Cache('127.0.0.1',8)
+
+@app.route("/")
+def index():
+    return render_template("index.html",**locals())
+
+@manager.option('-ip',help="input ip")
+def init(ip):
+    count_item = cache.get("count")
+    if count_item:
+        count = count_item["v"]
+    else:
+        count = 0
+    cache.set("ip_%s"%count,{"ip":ip,"last_take":None,"live":True})
+    cache.set("count",{"v":count+1})
+    print "add %s ok"%ip
+
+@manager.command
+def serv():
+    pass
--- utils.py
+++ utils.py
@@ -1,2 +1,43 @@
 #coding=utf8
 __author__ = 'alex'
+import redis
+import json
+
+class Cache(object):
+    def __init__(self,host,db):
+        self.redis = redis.Redis(host=host,port=6379,db=db)
+
+    def _gen_key(self,key,pub=False):
+        return "publish_%s"%key if pub else "object_%s"%key
+
+    def get(self, name, loads = json.loads):
+        data = self.redis.get(self._gen_key(name))
+        if data:
+            return loads(data)
+
+    def set(self, name, value, ttl=3600, dumps = json.dumps):
+        if ttl:
+            return self.redis.setex(self._gen_key(name), dumps(value), ttl)
+        return self.redis.set(self._gen_key(name), dumps(value))
+
+    def __getitem__(self, item):
+        return self.get(item)
+
+    def __setitem__(self, key, value):
+        return self.set(key,value,ttl=0)
+
+    def delete(self, key):
+        self.redis.delete(self._gen_key(key))
+
+
+    def subscribe(self, channels):
+        self.pubsub = self.redis.pubsub()
+        self.pubsub.subscribe(channels)
+
+    def publish(self, channel, message):
+        self.redis.publish(channel, json.dumps(message))
+
+    def listen(self, on_message):
+        for message in self.pubsub.listen():
+            if message.get("type") == "message":
+                on_message(message.get("channel"), json.loads(message.get("data")))
